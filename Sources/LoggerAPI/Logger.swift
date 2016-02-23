f615ed8955c8b4f4dbcacd2176caea64a530564e
iida <hayato.iida@speee.jp>
2016-02-23 12:16:34 +0900

    change __FUNCTION__ to #function

--- Sources/LoggerAPI/Logger.swift
+++ Sources/LoggerAPI/Logger.swift
@@ -34,31 +34,31 @@ public class Log {
     public static var logger: Logger?
     
 
-    public static func verbose(msg: String, functionName: String = __FUNCTION__,
+    public static func verbose(msg: String, functionName: String = #function,
         lineNum: Int = __LINE__, fileName: String = __FILE__ ) {
             logger?.log( .Verbose, msg: msg,
                 functionName: functionName, lineNum: lineNum, fileName: fileName)
     }
     
-    public class func info(msg: String, functionName: String = __FUNCTION__,
+    public class func info(msg: String, functionName: String = #function,
         lineNum: Int = __LINE__, fileName: String = __FILE__) {
             logger?.log( .Info, msg: msg,
                 functionName: functionName, lineNum: lineNum, fileName: fileName)
     }
     
-    public class func warning(msg: String, functionName: String = __FUNCTION__,
+    public class func warning(msg: String, functionName: String = #function,
         lineNum: Int = __LINE__, fileName: String = __FILE__) {
             logger?.log( .Warning, msg: msg,
                 functionName: functionName, lineNum: lineNum, fileName: fileName)
     }
     
-    public class func error(msg: String, functionName: String = __FUNCTION__,
+    public class func error(msg: String, functionName: String = #function,
         lineNum: Int = __LINE__, fileName: String = __FILE__) {
             logger?.log( .Error, msg: msg,
                 functionName: functionName, lineNum: lineNum, fileName: fileName)
     }
     
-    public class func debug(msg: String, functionName: String = __FUNCTION__,
+    public class func debug(msg: String, functionName: String = #function,
         lineNum: Int = __LINE__, fileName: String = __FILE__) {
             logger?.log( .Warning, msg: msg,
                 functionName: functionName, lineNum: lineNum, fileName: fileName)
