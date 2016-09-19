49830a63c4e96136ff4891b2bfee96358bbd2fad
Shmuel Kallner <kallner@il.ibm.com>
2016-09-19 16:52:10 +0300

    Added API documentation and documentation generation (#13)
    * IBM-Swift/Kitura#726 Added API documentation and documentation generation
    
    * IBM-Swift/Kitura#726 Updates after review.
    
    * IBM-Swift/Kitura#726 Added generated documentation.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # LoggerAPI
-Provides a logger protocol for Logger implementations.
+A logger protocol implemented by Logger implementations. In addition, a class with a set
+of static functions for logging within your code is provided.
+
+Kitura uses this API throughout its implementation when logging.
+ 
--- Sources/LoggerAPI/Logger.swift
+++ Sources/LoggerAPI/Logger.swift
@@ -14,17 +14,29 @@
  * limitations under the License.
  **/
 
+/// The type of a particular log message. Passed with the message to be logged to the
+/// actual logger implementation. It is also used to enable filtering of the log based
+/// on minimal type to log.
 public enum LoggerMessageType: Int {
+    /// Log message type for logging entering into a function
     case entry = 1
+    /// Log message type for logging exiting from a function
     case exit = 2
+    /// Log message type for logging a debugging message
     case debug = 3
+    /// Log message type for logging messages in verbose mode
     case verbose = 4
+    /// Log message type for logging an informational message
     case info = 5
+    /// Log message type for logging a warning message
     case warning = 6
+    /// Log message type for logging an error message
     case error = 7
 }
 
+/// Implement the `CustomStringConvertible` protocol for the `LoggerMessageType` enum
 extension LoggerMessageType: CustomStringConvertible {
+    /// Convert a `LoggerMessageType` into a pritable format
     public var description: String {
         switch self {
         case .entry:
@@ -45,62 +57,173 @@ extension LoggerMessageType: CustomStringConvertible {
     }
 }
 
+/// A logger protocol implemented by Logger implementations. This API is used by Kitura
+/// throughout its implementation when logging.
 public protocol Logger {
 
+    /// Output a logged message.
+    ///
+    /// - Parameter type: The type of the message (`LoggerMessageType`) being logged.
+    /// - Parameter msg: The message to be logged
+    /// - Parameter functionName: The name of the function invoking the logger API.
+    /// - Parameter lineNum: The line in the source code of the function invoking the
+    ///                     logger API.
+    /// - Parameter fileName: The file of the source code of the function invoking the
+    ///                      logger API.
     func log(_ type: LoggerMessageType, msg: String,
         functionName: String, lineNum: Int, fileName: String)
     
+    /// A function that will indicate if a message with a specified type (`LoggerMessageType`)
+    /// will be outputted in the log (i.e. will not be filtered out).
+    ///
+    /// -Parameter type: The type of message that one wants to know if it will be output in the log.
+    ///
+    /// - Returns: A Bool indicating whether, if true, or not a message of the specified type
+    ///           (`LoggerMessageType`) will be outputted.
     func isLogging(_ level: LoggerMessageType) -> Bool
 
 }
 
+/// A class of static members used by anyone who wants to log mesages.
 public class Log {
 
+    /// An instance of the logger. It should usually be the one and only reference
+    /// of the actual `Logger` protocol implementation in the system.
     public static var logger: Logger?
 
-
+    /// Log a log message for use when in verbose logging mode.
+    ///
+    /// - Parameter msg: The message to be logged
+    /// - Parameter functionName: The name of the function invoking the logger API.
+    ///                          Defaults to the actual name of the function invoking
+    ///                          this function.
+    /// - Parameter lineNum: The line in the source code of the function invoking the
+    ///                     logger API. Defaults to the actual line of the actual
+    ///                     function invoking this function.
+    /// - Parameter fileName: The file of the source code of the function invoking the
+    ///                      logger API. Defaults to the file of the actual function
+    ///                      invoking this function.
     public static func verbose(_ msg: String, functionName: String = #function,
         lineNum: Int = #line, fileName: String = #file ) {
             logger?.log( .verbose, msg: msg,
                 functionName: functionName, lineNum: lineNum, fileName: fileName)
     }
 
+    /// Log an informational message.
+    ///
+    /// - Parameter msg: The message to be logged
+    /// - Parameter functionName: The name of the function invoking the logger API.
+    ///                          Defaults to the actual name of the function invoking
+    ///                          this function.
+    /// - Parameter lineNum: The line in the source code of the function invoking the
+    ///                     logger API. Defaults to the actual line of the actual
+    ///                     function invoking this function.
+    /// - Parameter fileName: The file of the source code of the function invoking the
+    ///                      logger API. Defaults to the file of the actual function
+    ///                      invoking this function.
     public class func info(_ msg: String, functionName: String = #function,
         lineNum: Int = #line, fileName: String = #file) {
             logger?.log( .info, msg: msg,
                 functionName: functionName, lineNum: lineNum, fileName: fileName)
     }
 
+    /// Log a warning message.
+    ///
+    /// - Parameter msg: The message to be logged
+    /// - Parameter functionName: The name of the function invoking the logger API.
+    ///                          Defaults to the actual name of the function invoking
+    ///                          this function.
+    /// - Parameter lineNum: The line in the source code of the function invoking the
+    ///                     logger API. Defaults to the actual line of the actual
+    ///                     function invoking this function.
+    /// - Parameter fileName: The file of the source code of the function invoking the
+    ///                      logger API. Defaults to the file of the actual function
+    ///                      invoking this function.
     public class func warning(_ msg: String, functionName: String = #function,
         lineNum: Int = #line, fileName: String = #file) {
             logger?.log( .warning, msg: msg,
                 functionName: functionName, lineNum: lineNum, fileName: fileName)
     }
 
+    /// Log an error message.
+    ///
+    /// - Parameter msg: The message to be logged
+    /// - Parameter functionName: The name of the function invoking the logger API.
+    ///                          Defaults to the actual name of the function invoking
+    ///                          this function.
+    /// - Parameter lineNum: The line in the source code of the function invoking the
+    ///                     logger API. Defaults to the actual line of the actual
+    ///                     function invoking this function.
+    /// - Parameter fileName: The file of the source code of the function invoking the
+    ///                      logger API. Defaults to the file of the actual function
+    ///                      invoking this function.
     public class func error(_ msg: String, functionName: String = #function,
         lineNum: Int = #line, fileName: String = #file) {
             logger?.log( .error, msg: msg,
                 functionName: functionName, lineNum: lineNum, fileName: fileName)
     }
 
+    /// Log a debuging message.
+    ///
+    /// - Parameter msg: The message to be logged
+    /// - Parameter functionName: The name of the function invoking the logger API.
+    ///                          Defaults to the actual name of the function invoking
+    ///                          this function.
+    /// - Parameter lineNum: The line in the source code of the function invoking the
+    ///                     logger API. Defaults to the actual line of the actual
+    ///                     function invoking this function.
+    /// - Parameter fileName: The file of the source code of the function invoking the
+    ///                      logger API. Defaults to the file of the actual function
+    ///                      invoking this function.
     public class func debug(_ msg: String, functionName: String = #function,
         lineNum: Int = #line, fileName: String = #file) {
             logger?.log( .debug, msg: msg,
                 functionName: functionName, lineNum: lineNum, fileName: fileName)
     }
     
+    /// Log a message when entering a function.
+    ///
+    /// - Parameter msg: The message to be logged
+    /// - Parameter functionName: The name of the function invoking the logger API.
+    ///                          Defaults to the actual name of the function invoking
+    ///                          this function.
+    /// - Parameter lineNum: The line in the source code of the function invoking the
+    ///                     logger API. Defaults to the actual line of the actual
+    ///                     function invoking this function.
+    /// - Parameter fileName: The file of the source code of the function invoking the
+    ///                      logger API. Defaults to the file of the actual function
+    ///                      invoking this function.
     public class func entry(_ msg: String, functionName: String = #function,
         lineNum: Int = #line, fileName: String = #file) {
             logger?.log(.entry, msg: msg,
                 functionName: functionName, lineNum: lineNum, fileName: fileName)
     }
     
+    /// Log a message when exiting a function.
+    ///
+    /// - Parameter msg: The message to be logged
+    /// - Parameter functionName: The name of the function invoking the logger API.
+    ///                          Defaults to the actual name of the function invoking
+    ///                          this function.
+    /// - Parameter lineNum: The line in the source code of the function invoking the
+    ///                     logger API. Defaults to the actual line of the actual
+    ///                     function invoking this function.
+    /// - Parameter fileName: The file of the source code of the function invoking the
+    ///                      logger API. Defaults to the file of the actual function
+    ///                      invoking this function.
     public class func exit(_ msg: String, functionName: String = #function,
         lineNum: Int = #line, fileName: String = #file) {
             logger?.log(.exit, msg: msg,
                 functionName: functionName, lineNum: lineNum, fileName: fileName)
     }
     
+    /// A function that will indicate if a message with a specified type (`LoggerMessageType`)
+    /// will be outputted in the log (i.e. will not be filtered out).
+    ///
+    /// - Parameter type: The type of message that one wants to know if it will be output in the log.
+    ///
+    /// - Returns: A Bool indicating whether, if true, or not a message of the specified type
+    ///           (`LoggerMessageType`) will be outputted.
     public class func isLogging(_ level: LoggerMessageType) -> Bool {
         guard let logger = logger else {
             return false
