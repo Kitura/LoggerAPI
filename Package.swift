5c97588f3a1eed7fcd5a7c845ab277cb9724b4f0
ShihabMehboob <shihab.mehboob1@ibm.com>
2018-01-30 14:43:57 +0000

    Updates to swift 4.1

--- Package.swift
+++ Package.swift
@@ -21,18 +21,9 @@ import PackageDescription
 
 let package = Package(
     name: "LoggerAPI",
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "LoggerAPI",
-            targets: ["LoggerAPI"]
-        )
-    ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "LoggerAPI"
-        )
+        Target(
+            name: "LoggerAPI",
+            dependencies: []),
     ]
 )
