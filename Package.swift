7870880f7aef495272273fa8b118c781a666a944
ShihabMehboob <shihab.mehboob1@ibm.com>
2018-01-30 14:39:40 +0000

    Updates to swift 4.1

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.1
+// swift-tools-version:4.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 /**
@@ -21,9 +21,18 @@ import PackageDescription
 
 let package = Package(
     name: "LoggerAPI",
-    targets: [
-        Target(
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
             name: "LoggerAPI",
-            dependencies: []),
+            targets: ["LoggerAPI"]
+        )
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "LoggerAPI"
+        )
     ]
 )
