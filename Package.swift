52c31b66044801d96b620afe4c5328993682f616
David Jones <djones6dev@gmail.com>
2018-02-23 15:47:13 +0000

    #25: Revert breakage from #24 (#26)

--- Package.swift
+++ Package.swift
@@ -21,9 +21,18 @@ import PackageDescription
 
 let package = Package(
     name: "LoggerAPI",
-    targets: [
-        .target(
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
             name: "LoggerAPI",
-            dependencies: []),
+            targets: ["LoggerAPI"]
+        )
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "LoggerAPI"
+        )
     ]
 )
