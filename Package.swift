Kitura/LoggerAPI

commit 14ef7c4b73a25b770824e290eee7df4290471b18

Update to Swift 4.1 (#24)

Package.swift
@@ -1,3 +1,6 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 /**
  * Copyright IBM Corporation 2016, 2017
  *
@@ -19,7 +22,7 @@ import PackageDescription
 let package = Package(
     name: "LoggerAPI",
     targets: [
-        Target(
+        .target(
             name: "LoggerAPI",
             dependencies: []),
     ]

.swift-version
@@ -1,1 +1,1 @@
-3.1.1
+4.0.3

Package@swift-4.0.swift
@@ -1,38 +1,0 @@
-// swift-tools-version:4.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
-/**
- * Copyright IBM Corporation 2016, 2017
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- **/
-
-import PackageDescription
-
-let package = Package(
-    name: "LoggerAPI",
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "LoggerAPI",
-            targets: ["LoggerAPI"]
-        )
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "LoggerAPI"
-        )
-    ]
-)

commit 8c5dedcee059466422e03495e61adf69e38fc042

#25: Revert breakage from #24

Package.swift
@@ -21,9 +21,18 @@ import PackageDescription
 
 let package = Package(
     name: "LoggerAPI",
-    targets: [
-        .target(
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
             name: "LoggerAPI",
-            dependencies: []),
+            targets: ["LoggerAPI"]
+        )
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "LoggerAPI"
+        )
     ]
 )