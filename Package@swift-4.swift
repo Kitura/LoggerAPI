ee0084cbb8bace59b3409cec05bedc5d2e97f8b7
David Jones <djones6dev@gmail.com>
2018-02-27 16:47:27 +0000

    Restore Swift 3.1.1 support (#27)
    - and enable Travis

--- Package@swift-4.swift
+++ Package@swift-4.swift
@@ -1,3 +1,6 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 /**
  * Copyright IBM Corporation 2016, 2017
  *
@@ -18,9 +21,18 @@ import PackageDescription
 
 let package = Package(
     name: "LoggerAPI",
-    targets: [
-        Target(
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
             name: "LoggerAPI",
-            dependencies: []),
+            targets: ["LoggerAPI"]
+        )
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "LoggerAPI"
+        )
     ]
 )
